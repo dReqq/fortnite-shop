4bb77d103180e93ad1a8d0ff097a362b8cfaa696
Den <denis.panfilov4@gmail.com>
2023-01-30 15:24:10 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a className="grey-text text-lighten-4 right" href="https://github.com/dReqq/fortnite-shop" target='_blank' rel="noreferrer">Repo</a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="#ba68c8 purple lighten-2">
         <div classNameName="nav-wrapper">
-        <a href="!#" className="brand-logo">Fortnite Shop</a>
+        <a href="/" className="brand-logo">Fortnite Shop</a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
-            <li><a href="!#">Repo</a></li>
+            <li><a href="https://github.com/dReqq/fortnite-shop" target='_blank' rel="noreferrer">Repo</a></li>
         </ul>
         </div>
     </nav>
